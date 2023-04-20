26fee4132fa9c68f8e04a83920c3e51d8ff01298
s1dn31 <sidneieckhardt@gmail.com>
2023-04-19 21:54:15 -0300

    variáveis

---
+++ index.js
@@ -0,0 +1,63 @@
+//prompt("digite algo")
+// const nomePessoa= "josé"
+
+// console.log(nomePessoa)
+// let nomePessoa1= "pedro"
+// nomePessoa1="maria"
+// console.log(nomePessoa1)
+
+// const seuNome= prompt ("digite seu nome")
+// const suaIdade= Number (prompt ("digite sua idade"))
+// console.log(seuNome, suaIdade)
+// console.log(typeof seuNome)
+// console.log(typeof suaIdade)
+
+//exercício 1
+// let a=10
+// let b=10
+// console.log(b)= vai mostrar 10
+// b = 5
+// console.log(a,b)= vai mostrar (10,5)
+
+//exer 2
+
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c) vai mostrar (10,10,10)
+
+// exer 3
+// let horasTrabalhadasPorDia = prompt("Quantas horas você trabalha por dia?")
+// let valorDaDiária = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${horasTrabalhadasPorDia/valorDaDiária} por hora`)
+
+//exer 4 code
+
+let incluirNome
+let incluirIdade
+console.log(typeof incluirNome)
+console.log(typeof incluirIdade)
+
+// se declarar a variável como "const",vai ser apresentado o erro no console porque toda "const" deve ser
+//inicializada.
+// se usar "let" o console ira mostrar Undefined! por ser uma variável que ainda não foi definida
+
+const nome="jose"
+const idade="20"
+console.log("Olá", nome, ",você tem", idade,"anos")
+
+//três perguntas com respostas sim ou não
+
+const almoçou= "Sim"
+const treinou="sim"
+const acordou="sim"
+console.log("você almoçou hoje?", almoçou)
+console.log("voçê treinou hoje?", treinou)
+console.log("voçê acordou cedo?",acordou)
+
+//pasando os valores de a para b e de b para a
+//let a=10
+//let b=25
