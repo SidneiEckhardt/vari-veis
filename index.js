3864a855633530073bcae2c97c949a70e1568782
s1dn31 <sidneieckhardt@gmail.com>
2023-04-20 20:23:46 -0300

    exerciciosvariaveis

--- index.js
+++ index.js
@@ -31,33 +31,51 @@
 
 // exer 3
 // let horasTrabalhadasPorDia = prompt("Quantas horas você trabalha por dia?")
-// let valorDaDiária = prompt("Quanto você recebe por dia?")
-// alert(`Voce recebe ${horasTrabalhadasPorDia/valorDaDiária} por hora`)
+//  let valorDaDiária = prompt("Quanto você recebe por dia?")
+//  alert(`Voce recebe ${horasTrabalhadasPorDia/valorDaDiária} por hora`)
 
 //exer 4 code
 
-let incluirNome
-let incluirIdade
-console.log(typeof incluirNome)
-console.log(typeof incluirIdade)
+// let incluirNome
+// let incluirIdade
+// console.log(typeof incluirNome)
+// console.log(typeof incluirIdade)
+// incluirNome= prompt("digite seu nome")
+// incluirIdade= prompt("digite sua idade")
+// console.log(typeof incluirNome)
+// console.log(typeof incluirIdade)
 
 // se declarar a variável como "const",vai ser apresentado o erro no console porque toda "const" deve ser
 //inicializada.
 // se usar "let" o console ira mostrar Undefined! por ser uma variável que ainda não foi definida
 
-const nome="jose"
-const idade="20"
-console.log("Olá", nome, ",você tem", idade,"anos")
+// const nome="jose"
+// const idade="20"
+// console.log("Olá", nome, ",você tem", idade,"anos")
 
 //três perguntas com respostas sim ou não
 
-const almoçou= "Sim"
-const treinou="sim"
-const acordou="sim"
-console.log("você almoçou hoje?", almoçou)
-console.log("voçê treinou hoje?", treinou)
-console.log("voçê acordou cedo?",acordou)
+// const almoçou= prompt("você almoçou hoje?")
+// const treinou= prompt("você treinou hoje?")
+// const acordou= prompt("você acordou cedo hoje?")
+// console.log("você almoçou hoje?- ", almoçou)
+// console.log("voçê treinou hoje?- ", treinou)
+// console.log("voçê acordou cedo?- ",acordou)
 
 //pasando os valores de a para b e de b para a
-//let a=10
-//let b=25
+// let a= Number(prompt("digite 1 numero"))
+// let b= Number(prompt("digite um numero"))
+// //  a=10
+// //  b=25
+// c=a
+// a=b
+// b=c
+// console.log("valor de A",a)
+// console.log("valor de B",b)
+
+
+const primeiroNumero= Number (prompt("digite 1 numero"))
+const segundoNumero= Number (prompt("digite outro numero"))
+
+console.log("A soma é", primeiroNumero + segundoNumero);
+console.log("A multiplicação é", primeiroNumero * segundoNumero);
